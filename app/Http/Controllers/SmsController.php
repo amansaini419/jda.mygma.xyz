a6af13985b56608288557d5d0a1bd04d6b9304ff
Aman Saini <amansaini87@rediffmail.com>
2024-02-07 21:17:23 +0530

    checking sms

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -2,31 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 
 class SmsController extends Controller
 {
     public static function send($countryCode, $mobileNumber, $content)
     {
-        $query = array(
-			"clientid" => env('HUBTEL_CLIENT_ID'),
-			"clientsecret" => env('HUBTEL_CLIENT_SECRET'),
-			"from" => env('HUBTEL_SENDER_NICKNAME'),
-			"to" => $countryCode . (int)$mobileNumber,
-			"content" => $content
-		);
+        try{
+            $query = array(
+                "clientid" => env('HUBTEL_CLIENT_ID'),
+                "clientsecret" => env('HUBTEL_CLIENT_SECRET'),
+                "from" => env('HUBTEL_SENDER_NICKNAME'),
+                "to" => $countryCode . (int)$mobileNumber,
+                "content" => $content
+            );
 
-		$curl = curl_init();
+            $curl = curl_init();
 
-		curl_setopt_array($curl, [
-			CURLOPT_URL => "https://smsc.hubtel.com/v1/messages/send?" . http_build_query($query),
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_CUSTOMREQUEST => "GET",
-		]);
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://smsc.hubtel.com/v1/messages/send?" . http_build_query($query),
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CUSTOMREQUEST => "GET",
+            ]);
 
-		/* $response = curl_exec($curl);
-		$error = curl_error($curl); */
-        curl_exec($curl);
-		curl_close($curl);
+            /* $response = curl_exec($curl);
+            $error = curl_error($curl); */
+            curl_exec($curl);
+            curl_close($curl);
+        }
+        catch(Exception $exp){
+            dd($exp->getMessage());
+        }
     }
 }
