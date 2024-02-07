822e766c691f0607762c64c6be4ab03d1d8498ea
Aman Saini <amansaini87@rediffmail.com>
2024-02-07 20:45:05 +0530

    uncomented sms on login

--- app/Services/VoterService.php
+++ app/Services/VoterService.php
@@ -44,8 +44,8 @@ class VoterService
 
         /* Mail::to($voter->email)->send(new ConfirmCodeEmail($voter, $confirmCode));
 
-        $message = "Hello $voter->first_name,\nYour 2FA to verify your identity to cast your vote is $confirmCode.\nDo not share it with anyone.";
-        SmsController::send($voter->country_code, $voter->mobile_number, $message); */
+        $message = "Hello $voter->first_name,\nYour 2FA to verify your identity to cast your vote is $confirmCode.\nDo not share it with anyone."; */
+        SmsController::send($voter->country_code, $voter->mobile_number, $message);
         Mail::to($voter->email)->send(new LoginCodeEmail($voter, $confirmCode));
 
         $message = "Hello $voter->first_name,\nYour OTP to verify your identity to cast your vote is $confirmCode.\nDo not share it with anyone.";
