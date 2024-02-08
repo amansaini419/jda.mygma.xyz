984667cab4721f8e82e61c2c6903b57bc3aade9c
Aman Saini <amansaini87@rediffmail.com>
2024-02-08 18:55:46 +0530

    making table responsive

--- resources/views/pages/summary.blade.php
+++ resources/views/pages/summary.blade.php
@@ -11,54 +11,56 @@
     <div class="row g-4">
         <div class="col-md-12">
             <div class="card rounded-3">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th class="text-uppercase">Category</th>
-                            <th class="text-uppercase">Selected</th>
-                            <th class="text-uppercase">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($selectedCandidates as $selectedCandidate)
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
                             <tr>
-                                <td class="fw-bold text-uppercase">{{ $selectedCandidate->category->name }}</td>
-                                <td>
-                                    <div class="d-flex">
-                                        <div class="border border-light rounded-circle bg-light" style="width: 50px; height: 50px;">
-                                            <img src="{{ Storage::url($selectedCandidate->nominee->image) }}" class="nominee-img img-fluid rounded-circle w-100 h-100" alt="{{ $selectedCandidate->nominee->name }}" />
+                                <th class="text-uppercase">Category</th>
+                                <th class="text-uppercase">Selected</th>
+                                <th class="text-uppercase">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($selectedCandidates as $selectedCandidate)
+                                <tr>
+                                    <td class="fw-bold text-uppercase">{{ $selectedCandidate->category->name }}</td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <div class="border border-light rounded-circle bg-light" style="width: 50px; height: 50px;">
+                                                <img src="{{ Storage::url($selectedCandidate->nominee->image) }}" class="nominee-img img-fluid rounded-circle w-100 h-100" alt="{{ $selectedCandidate->nominee->name }}" />
+                                            </div>
+                                            <div class="ms-2">
+                                                <h4 class="fw-bold mb-1 text-uppercase fs-5">{{ $selectedCandidate->nominee->name }}</h4>
+                                                <blockquote class="blockquote small mb-0">
+                                                    <p class="mb-0"><em>{{ $selectedCandidate->nominee->tagline }}</em></p>
+                                                </blockquote>
+                                            </div>
                                         </div>
-                                        <div class="ms-2">
-                                            <h4 class="fw-bold mb-1 text-uppercase fs-5">{{ $selectedCandidate->nominee->name }}</h4>
-                                            <blockquote class="blockquote small mb-0">
-                                                <p class="mb-0"><em>{{ $selectedCandidate->nominee->tagline }}</em></p>
-                                            </blockquote>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    @if($isVoted)
-                                        <h4 class="text-uppercase fw-medium fs-6 mb-0"><span class="text-success">voted <i class="ti ti-check"></i></span></h4>
-                                    @else
-                                        <a href="{{ route('category', ['slug' => $selectedCandidate->category->slug]) }}" class="btn btn-primary btn-lg rounded-pill text-uppercase fw-medium">change selection</a>
+                                    </td>
+                                    <td>
+                                        @if($isVoted)
+                                            <h4 class="text-uppercase fw-medium fs-6 mb-0"><span class="text-success">voted <i class="ti ti-check"></i></span></h4>
+                                        @else
+                                            <a href="{{ route('category', ['slug' => $selectedCandidate->category->slug]) }}" class="btn btn-primary btn-lg rounded-pill text-uppercase fw-medium">change selection</a>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td class="border-0">
+                                    @if($isSelectedAll && !$isVoted)
+                                        <form action="{{ route('vote') }}" method="post" class="mx-auto mb-4 text-center">
+                                            @csrf
+                                            <button type="submit" class="btn btn-warning btn-lg rounded-pill text-uppercase fw-medium">CONFIRM AND SUBMIT VOTES</button>
+                                        </form>
                                     @endif
                                 </td>
                             </tr>
-                        @endforeach
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td class="border-0">
-                                @if($isSelectedAll && !$isVoted)
-                                    <form action="{{ route('vote') }}" method="post" class="mx-auto mb-4 text-center">
-                                        @csrf
-                                        <button type="submit" class="btn btn-warning btn-lg rounded-pill text-uppercase fw-medium">CONFIRM AND SUBMIT VOTES</button>
-                                    </form>
-                                @endif
-                            </td>
-                        </tr>
-                    </tfoot>
-                </table>
+                        </tfoot>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
